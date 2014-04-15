5dd5d8c4fecf903afacf8aa803b227889a533c8e
webdev7x6 <dmccoy@amctheatres.com>
2014-04-14 20:26:56 -0500

    removed missing files from project;  added publish folder;

---
+++ publish/Areas/CustomAdmin/common/js/news/news.js
@@ -0,0 +1,23 @@
+﻿(function ($, undefined) {
+    var hn = window.hn = window.hn || {};
+    (function () {
+
+        // entity descriptions
+        hn.itemDescription = 'News Articles';
+        hn.itemDeleteDescription = 'Are you sure you want to delete this news article?';
+
+        // entity CRUD uperation URLs
+        hn.getAllItemsURL = '/CustomAdmin/News/AllArticlesAjax';
+        hn.findItemsURL = '/CustomAdmin/News/FindArticlesAjax';
+        hn.editItemURL = '/CustomAdmin/News/EditArticleAjax';
+        hn.deleteItemURL = '/CustomAdmin/News/DeleteArticleAjax';
+        hn.editInPlaceURL = '/CustomAdmin/News/EditArticleAjax';
+
+        // this is the init function called at the end of the hn-item-index.js page ready function
+        // put all your custom methods here
+        hn.items.index.pageInit = function () {
+
+        };
+
+    })();
+})(jQuery);
