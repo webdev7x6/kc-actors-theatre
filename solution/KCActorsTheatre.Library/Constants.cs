e49b991ee76b8f22c896768ac1fd317bbd248305
Matt Lawson <matt.lawson@clickfarminteractive.com>
2014-04-30 14:23:32 -0500

    production config settings.

--- solution/KCActorsTheatre.Library/Constants.cs
+++ solution/KCActorsTheatre.Library/Constants.cs
@@ -12,7 +12,7 @@ namespace KCActorsTheatre
         #region Machine names
 
         public const string MachineName_Dev = "DEV";
-        public const string MachineName_Prod = "PROD";
+        public const string MachineName_Prod = "FE01";
 
         #endregion
     }
